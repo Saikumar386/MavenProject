d641667616fc07f5b2bf1cfeb96409d71216534c
Hp <Hp@DESKTOP-J1PLGDT>
2024-07-15 17:17:23 +0530

    after stashing

--- demo/src/main/java/com/nit/service/service.java
+++ demo/src/main/java/com/nit/service/service.java
@@ -4,11 +4,20 @@ public class service {
    
 	public void service()
 	{
-		//hgh
+		
 		System.out.println("service.service()");
 	}
 	public void m2()
 	{
 		System.out.println("service.m2()");
 	}
+
+	
+	public void m1()
+	{
+		
+	}
+	
+	
+
 }
