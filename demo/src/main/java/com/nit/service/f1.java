d3a6f89ef658c7564e6c609087a1fc8f541cec74
Hp <Hp@DESKTOP-J1PLGDT>
2024-07-15 16:41:50 +0530

    devloper add f1,f2 files

---
+++ demo/src/main/java/com/nit/service/f1.java
@@ -0,0 +1,9 @@
+package com.nit.service;
+
+public class f1 {
+
+	public void f1()
+	{
+		System.out.println("f1.f1()");
+	}
+}
---
+++ demo/src/main/java/com/nit/service/f2.java
@@ -0,0 +1,9 @@
+package com.nit.service;
+
+public class f2 {
+   
+	public void f2()
+	{
+		System.out.println("f2.f2()");
+	}
+}
